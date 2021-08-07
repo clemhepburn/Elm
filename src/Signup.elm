03671a8b7946d4d924054eb374635d41d489dea4
Clem Hepburn <clemhepburn@gmail.com>
2021-08-07 11:20:34 -0700

    more exercises including RandomNumber

--- src/Signup.elm
+++ src/Signup.elm
@@ -1,49 +1,126 @@
-module Signup exposing (User)
+module Signup exposing (main)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (onClick, onInput)
+import Browser
 
 
 type alias User =
-  { name : String
-  , email : String
-  , password : String
-  , loggedIn : Bool
-  }
+    { name : String
+    , email : String
+    , password : String
+    , loggedIn : Bool
+    }
+
 
 initialModel : User
 initialModel =
-  { name = ""
-  , email = ""
-  , password = ""
-  , loggedIn = False
-  }
+    { name = ""
+    , email = ""
+    , password = ""
+    , loggedIn = False
+    }
 
-view : User -> Html msg
+
+view : User -> Html Msg
 view user =
     div []
-        [ h1 [] [ text "Sign up" ]
-        , Html.form []
+        [ h1 [ css [ paddingLeft (cm 3) ] ] [ text "Sign up" ]
+        , styledForm []
             [ div []
                 [ text "Name"
-                , input [ id "name", type_ "text" ] []
+                , styledInput 
+                  [ id "name"
+                  , type_ "text"
+                  , onInput SaveName ] []
                 ]
             , div []
                 [ text "Email"
-                , input [ id "email", type_ "email" ] []
+                , styledInput 
+                  [ id "email"
+                  , type_ "email"
+                  , onInput SaveEmail ] []
                 ]
             , div []
                 [ text "Password"
-                , input [ id "password", type_ "password" ] []
+                , styledInput 
+                  [ id "password"
+                  , type_ "password"
+                  , onInput SavePassword ] []
                 ]
             , div []
-                [ button [ type_ "submit" ]
+                [ styledButton 
+                [ type_ "submit"
+                , onClick Signup ]
                     [ text "Create my account" ]
                 ]
             ]
         ]
 
 
-main : Html msg
+styledForm : List (Attribute msg) -> List (Html msg) -> Html msg
+styledForm =
+    styled Html.Styled.form
+        [ borderRadius (px 5)
+        , backgroundColor (hex "#f2f2f2")
+        , padding (px 20)
+        , Css.width (px 300)
+        ]
+
+
+styledInput : List (Attribute msg) -> List (Html msg) -> Html msg
+styledInput =
+    styled Html.Styled.input
+        [ display block
+        , Css.width (px 260)
+        , padding2 (px 12) (px 20)
+        , margin2 (px 8) (px 0)
+        , border (px 0)
+        , borderRadius (px 4)
+        ]
+
+
+styledButton : List (Attribute msg) -> List (Html msg) -> Html msg
+styledButton =
+    styled Html.Styled.button
+        [ Css.width (px 300)
+        , backgroundColor (hex "#397cd5")
+        , color (hex "#fff")
+        , padding2 (px 14) (px 20)
+        , marginTop (px 10)
+        , border (px 0)
+        , borderRadius (px 4)
+        , fontSize (px 16)
+        ]
+
+type  Msg
+    = SaveName String
+    | SaveEmail String
+    | SavePassword String
+    | Signup
+
+update : Msg -> User -> User
+update message user =
+    case message of
+        SaveName name ->
+            { user | name = name  }
+        
+        SaveEmail email ->
+            { user | email = email }
+        
+        SavePassword password ->
+            { user | password = password }
+        
+        Signup ->
+            { user | loggedIn = True }
+
+
+main : Program () User Msg
 main =
- view initialModel
+    Browser.sandbox
+      { init = initialModel
+      , view = view >> toUnstyled
+      , update = update 
+      }
